f7af14ea2667c9c86e7d17cd945a8f47db7e97c8
birdtechstep <t.artsamart@gmail.com>
2019-12-20 21:08:59 +0700

    "kbide-esp-idf-platfrom-linuxarm"

---
+++ tools/xtensa-esp32-elf/lib/gcc/xtensa-esp32-elf/5.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/pi/crosstool-NG/.build/src/gcc-5.2.0/configure --build=armv7l-build_unknown-linux-gnueabihf --host=armv7l-build_unknown-linux-gnueabihf --target=xtensa-esp32-elf --prefix=/home/pi/crosstool-NG/builds/xtensa-esp32-elf --with-local-prefix=/home/pi/crosstool-NG/builds/xtensa-esp32-elf/xtensa-esp32-elf/sysroot --with-sysroot=/home/pi/crosstool-NG/builds/xtensa-esp32-elf/xtensa-esp32-elf/sysroot --with-newlib --enable-threads=no --disable-shared --with-pkgversion='crosstool-NG crosstool-ng-1.22.0-80-g6c4433a5-dirty' --disable-__cxa_atexit --enable-cxx-flags='-fno-rtti -ffunction-sections' --with-gmp=/home/pi/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-mpfr=/home/pi/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-mpc=/home/pi/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-isl=/home/pi/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-cloog=/home/pi/crosstool-NG/.build/xtensa-esp32-elf/buildtools --with-libelf=/home/pi/crosstool-NG/.build/xtensa-esp32-elf/buildtools --enable-lto --enable-target-optspace --without-long-double-128 --disable-libgomp --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-nls --enable-languages=c,c++ --disable-libstdcxx-verbose --enable-threads=posix --enable-gcov-custom-rtio";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
